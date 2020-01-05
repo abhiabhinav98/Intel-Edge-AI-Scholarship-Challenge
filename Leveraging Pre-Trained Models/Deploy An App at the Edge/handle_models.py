1b6939de8c46065880891c6f9530019b37c6e3e8
Abhinav Gupta <abhiabhinav98@gmail.com>
2020-01-05 11:57:01 +0530

    Add files via upload

---
+++ Leveraging Pre-Trained Models/Deploy An App at the Edge/handle_models.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+
+
+def handle_pose(output, input_shape):
+    '''
+    Handles the output of the Pose Estimation model.
+    Returns ONLY the keypoint heatmaps, and not the Part Affinity Fields.
+    '''
+    # TODO 1: Extract only the second blob output (keypoint heatmaps)
+    heatmaps = output['Mconv7_stage2_L2']
+    # TODO 2: Resize the heatmap back to the size of the input
+    out_heatmap = np.zeros([heatmaps.shape[1], input_shape[0], input_shape[1]])
+    #print(output.keys())
+    for h in range(len(heatmaps[0])):
+        out_heatmap[h] = cv2.resize(heatmaps[0][h], input_shape[0:2][::-1])
+    return out_heatmap
+    #link = output['model/link_logits_/add']
+    #segm = output['model/segm_logits/add']
+    
+    #link_shape = link.shape
+    #segm_shape = segm.shape
+    
+    #segm = segm.reshape(segm_shape[1], segm_shape[2], segm_shape[3])
+    #segm_shape = segm.shape
+    
+    #output = np.zeros([input_shape[2], input_shape[0], input_shape[1]])
+    #for c in range(segm_shape[0]):
+    #    output[c] = cv2.resize(segm[c], (input_shape[1], input_shape[0]))
+    
+    #return output
+
+
+def handle_text(output, input_shape):
+    '''
+    Handles the output of the Text Detection model.
+    Returns ONLY the text/no text classification of each pixel,
+        and not the linkage between pixels and their neighbors.
+    '''
+    # TODO 1: Extract only the first blob output (text/no text classification)
+    text_classes = output['model/segm_logits/add']
+    # TODO 2: Resize this output back to the size of the input
+    out_text = np.empty([text_classes.shape[1], input_shape[0], input_shape[1]])
+    for t in range(len(text_classes[0])):
+        out_text[t] = cv2.resize(text_classes[0][t], input_shape[0:2][::-1])
+    return out_text
+    #print(output.keys())
+    #link = output['Mconv7_stage_L1']
+    #heatmap = output['Mconv7_stage2_L2']
+    #heatmap_shape = heatmap.shape
+    #heatmap = heatmap.reshape(heatmap_shape[1], heatmap_shape[2], heatmap_shape[3])
+    
+    #heatmap_shape = heatmap.shape
+    #output = np.zeros([heatmap_shape[0], input_shape[0], input_shape[1]])
+    #for c in range(heatmap_shape[0]):
+    #    output[c] = cv2.resize(heatmap[c], (input_shape[1], input_shape[0]))
+
+    #return output
+
+
+def handle_car(output, input_shape):
+    '''
+    Handles the output of the Car Metadata model.
+    Returns two integers: the argmax of each softmax output.
+    The first is for color, and the second for type.
+    '''
+    # TODO 1: Get the argmax of the "color" output
+    # TODO 2: Get the argmax of the "type" output
+    #print(output.keys())
+    car_color = np.argmax(output['color'])
+    car_type = np.argmax(output['type'])
+
+    return car_color, car_type
+
+
+def handle_output(model_type):
+    '''
+    Returns the related function to handle an output,
+        based on the model_type being used.
+    '''
+    if model_type == "POSE":
+        return handle_pose
+    elif model_type == "TEXT":
+        return handle_text
+    elif model_type == "CAR_META":
+        return handle_car
+    else:
+        return None
+
+
+'''
+The below function is carried over from the previous exercise.
+You just need to call it appropriately in `app.py` to preprocess
+the input image.
+'''
+def preprocessing(input_image, height, width):
+    '''
+    Given an input image, height and width:
+    - Resize to width and height
+    - Transpose the final "channel" dimension to be first
+    - Reshape the image to add a "batch" of 1 at the start 
+    '''
+    image = np.copy(input_image)
+    image = cv2.resize(image, (width, height))
+    image = image.transpose((2,0,1))
+    image = image.reshape(1, 3, height, width)
+
+    return image
